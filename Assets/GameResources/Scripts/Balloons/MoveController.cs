9e9da999119bd41beef955313c30471394c54fd2
Nelly_Shal <elena89833156550@mai.ru>
2024-02-07 16:03:22 +0700

    Префабы шаров, картинки шаров, сцена (камера, небо, UI), контролер движения шаров

---
+++ Assets/GameResources/Scripts/Balloons/MoveController.cs
@@ -0,0 +1,93 @@
+namespace Balloons.Features.Balloons
+{
+    using System;
+    using System.Collections;
+    using UnityEngine;
+
+
+    /// <summary>
+    /// Перемещает объект в заданном направлении
+    /// </summary>
+    [RequireComponent((typeof(Rigidbody2D)))]
+    public class MoveController : MonoBehaviour
+    {
+        /// <summary>
+        /// Скорость движения объекта изменилась
+        /// </summary>
+        public event Action onSpeedChanged = delegate { };
+        /// <summary>
+        /// Направление движения объекта изменилось
+        /// </summary>
+        public event Action onDirectionChanged = delegate { };
+
+
+        /// <summary>
+        /// Скорость движения объекта
+        /// </summary>
+        public float Speed
+        {
+            get => speed;
+            set
+            {
+                if (value != speed)
+                {
+                    speed = value;
+                    onSpeedChanged();
+                }
+            }
+        }
+        /// <summary>
+        /// Нправление движения объекта
+        /// </summary>
+        public Vector2 Direction
+        {
+            get => direction;
+            set
+            {
+                if (value != direction)
+                {
+                    direction = value;
+                    onDirectionChanged();
+                }
+            }
+        }
+
+        [SerializeField]
+        protected float speed = 3f;
+
+        protected Vector2 direction = new Vector2(0, 1);
+        protected Rigidbody2D rb = default;
+        protected Coroutine coroutine = default;
+
+
+        protected virtual void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            Move();
+        }
+
+        /// <summary>
+        /// Задать движение объекту
+        /// </summary>
+        public virtual void Move()
+        {
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                coroutine = null;
+            }
+            coroutine = StartCoroutine(MoveCoroutine());
+
+        }
+
+        protected IEnumerator MoveCoroutine()
+        {
+            while (isActiveAndEnabled)
+            {
+
+                rb.velocity = direction * speed;
+                yield return null;
+            }
+        }
+    }
+}
