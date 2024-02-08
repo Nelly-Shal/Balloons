df661e6f6c892ebb4adc5e344e0b2e8b30c2e4e7
Nelly_Shal <elena89833156550@mai.ru>
2024-02-08 18:29:52 +0700

    Взамодейстие c шариками через IPointerDownHandler и изменение View

---
+++ Assets/GameResources/Scripts/Actions/ClickAction.cs
@@ -0,0 +1,43 @@
+﻿namespace Balloons.Features.Actions
+{
+    using System;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    /// <summary>
+    /// Проверяет количество кликов по объекту с целевым значением
+    /// </summary>
+    public class ClickAction : MonoBehaviour, IPointerDownHandler
+    {
+        /// <summary>
+        /// Событие о клике
+        /// </summary>
+        public event Action onClickAction = delegate { };
+        /// <summary>
+        /// Событие о достижении целевого значения
+        /// </summary>
+        public event Action onTargetCountClick = delegate { };
+
+        [SerializeField, Min(0)]
+        protected int targetCount = 1;
+
+        protected int count = 0;
+
+        protected virtual void OnEnable() => count = 0;
+
+        /// <summary>
+        /// Действие при клике на объект
+        /// </summary>
+        /// <param name="eventData"></param>
+        public virtual void OnPointerDown(PointerEventData eventData)
+        {
+            count++;
+            if (targetCount == count)
+            {
+                onTargetCountClick();
+                return;
+            }
+            onClickAction();
+        }
+    }
+}
---
+++ Assets/GameResources/Scripts/Actions/OnTargetClickView.cs
@@ -0,0 +1,72 @@
+﻿namespace Balloons.Features.Actions
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    /// <summary>
+    /// Обновляет View при достижении целевого значения
+    /// </summary>
+    [RequireComponent(typeof(ClickAction))]
+    public class OnTargetClickView : MonoBehaviour
+    {
+        [SerializeField]
+        protected List<GameObject> objectsToView = new List<GameObject>();
+        [SerializeField]
+        protected List<GameObject> objectsToHide = new List<GameObject>();
+
+        [SerializeField, Min(0)]
+        protected float delay = default;
+
+        protected ClickAction clickAction = default;
+        protected Coroutine coroutine = default;
+
+        protected virtual void Awake()
+        {
+            clickAction = GetComponent<ClickAction>();
+            clickAction.onTargetCountClick += UpdateView;
+        }
+
+        protected virtual void OnEnable() => UpdateListsView(false);
+
+        protected virtual void OnDestroy()
+        {
+            if (clickAction)
+            {
+                clickAction.onTargetCountClick -= UpdateView;
+            }
+        }
+
+        protected virtual void UpdateView()
+        {
+            UpdateListsView(true);
+            if (coroutine != null)
+            {
+                StopCoroutine(coroutine);
+                coroutine = null;
+            }
+            coroutine = StartCoroutine(WaitDelay());
+        }
+
+        protected virtual void UpdateListsView(bool status)
+        {
+            foreach (GameObject obj in objectsToView)
+            {
+                obj.SetActive(status);
+            }
+            foreach (GameObject obj in objectsToHide)
+            {
+                obj.SetActive(!status);
+            }
+        }
+
+        protected virtual IEnumerator WaitDelay()
+        {
+            while (isActiveAndEnabled)
+            {
+                yield return new WaitForSeconds(delay);
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
